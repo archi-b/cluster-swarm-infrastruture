698ca10b9352e16d38b8289d9919b800542597a1
Max J. Prass <max.prass@grupoboticario.com.br>
2022-09-26 11:07:34 -0300

    adding docker-compose to install

--- config/provision/docker.sh
+++ config/provision/docker.sh
@@ -7,9 +7,14 @@
 ## add repo
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-## install
+## install Docker-Ce
 sudo yum install -y docker-ce
 sudo systemctl enable docker
 sudo systemctl start docker
 
 sudo usermod -aG docker vagrant
+
+## Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
